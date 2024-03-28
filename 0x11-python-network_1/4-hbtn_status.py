d52b76704c374102671af5568ff44ee880e41720
David Agbesinyale <davidm7040@gmail.com>
2024-03-28 11:51:01 -0700

    correction

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status."""
+
 import requests
 
-if __name__ == "__main__":
 
-    url = "https://intranet.hbtn.io/status"
-    req = requests.get(url)
+if __name__ == "__main__":
+    req = requests.get('https://intranet.hbtn.io/status')
+
     print('Body response:')
-    print('\t- type: {}'.format(type(req.text)))
-    print('\t- content: {}'.format(req.text))
+    print('\t- type: {_type}'.format(_type=type(req.text)))
+    print('\t- content: {_content}'.format(_content=req.text))
